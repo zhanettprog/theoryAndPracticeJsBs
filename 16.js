c90819422c401950762f55a57e65055d8d0d0e2b
Zhanett Serikbayeva <109654778+zhanettprog@users.noreply.github.com>
2022-08-12 20:39:02 +0600

    Add files via upload

---
+++ 16.js
@@ -0,0 +1,11 @@
+const myCity = {
+  city: "New York",
+};
+myCity["popular"] = true;
+
+console.log(myCity);
+
+const countryProrertyName = "country";
+myCity[countryProrertyName] = "USA";
+
+console.log(myCity);
