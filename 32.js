94bcc47c6c743dd0f65494ae17fa36833a66859f
Zhanett Serikbayeva <109654778+zhanettprog@users.noreply.github.com>
2022-08-16 21:29:17 +0600

    Add files via upload

---
+++ 32.js
@@ -0,0 +1,20 @@
+const person = {
+  name: "Bob",
+  age: 21,
+  info: {
+    sex: "men",
+    city: "Los Angeles",
+    country: "USA",
+  },
+};
+
+const person2 = Object.assign({}, person);
+
+person2.name = "Mark";
+person2.info.city = "New York";
+
+console.log(person.info.city);
+console.log(person2.info.city);
+
+console.log(person);
+console.log(person2);
