b59fa456b6ba0027fbd0f2209d31a8581f4ff075
Zhanett Serikbayeva <109654778+zhanettprog@users.noreply.github.com>
2022-08-24 22:10:45 +0600

    Add files via upload

---
+++ 58.js
@@ -0,0 +1,16 @@
+const buttonInfo = {
+  text: "Buy",
+};
+
+const buttonStyle = {
+  color: "yellow",
+  width: 200,
+  height: 300,
+};
+
+const button = {
+  ...buttonInfo,
+  ...buttonStyle,
+};
+
+console.table(button);
