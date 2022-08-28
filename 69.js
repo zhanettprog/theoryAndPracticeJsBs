73b6472b93c501627b31ce8c3f1a46f036b88be1
Zhanett Serikbayeva <109654778+zhanettprog@users.noreply.github.com>
2022-08-28 22:00:40 +0600

    Add files via upload

---
+++ 69.js
@@ -0,0 +1,15 @@
+const newPost = (post, addedAt = Date()) => ({
+  ...post,
+  addedAt,
+});
+
+const firstPost = {
+  id: 1,
+  author: "Zhanett",
+};
+
+newPost(firstPost);
+
+const postFirst = newPost(firstPost);
+
+console.log();
