4094cb202e35bec968b733978eddeaef8adb681c
Zhanett Serikbayeva <109654778+zhanettprog@users.noreply.github.com>
2022-08-30 22:14:35 +0600

    Add files via upload

---
+++ 76.js
@@ -0,0 +1,11 @@
+const myArray = [1, true, "Zhanett"]; //undefined
+
+myArray; // (3) [1, true, 'Zhanett']
+
+myArray.length; // 3
+
+const myArray2 = [1, true, "Zhanett"]; //undefined
+
+myArray2; // (3) [1, true, 'Zhanett']
+
+myArray === myArray2; //false
