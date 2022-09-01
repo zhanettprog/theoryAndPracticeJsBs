ad66e8a1fc46e0bed451d50789944bac067f8d42
Zhanett Serikbayeva <109654778+zhanettprog@users.noreply.github.com>
2022-09-01 20:53:42 +0600

    Add files via upload

---
+++ 80.js
@@ -0,0 +1,12 @@
+const myArray = [1, 2, 3, 4];
+console.log(myArray);
+console.log(myArray.length);
+
+myArray[2] = "abc";
+console.log(myArray);
+console.log(myArray[2]);
+
+myArray[4] = true;
+
+console.log(myArray);
+console.log(myArray.length);
