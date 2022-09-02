b05fa4f7f2e9f4c1ff2850c170f9483b1006ddab
Zhanett Serikbayeva <109654778+zhanettprog@users.noreply.github.com>
2022-09-02 20:31:29 +0600

    Add files via upload

---
+++ 82.js
@@ -0,0 +1,15 @@
+const myArray = [1, 2, 3, 4];
+console.log(myArray);
+
+myArray.pop();
+console.log(myArray);
+
+const removedElement = myArray.pop();
+
+console.log(myArray);
+console.log(removedElement);
+
+const removedElement2 = myArray.pop();
+
+console.log(myArray);
+console.log(removedElement2);
