fefa0112f696822519cd18c1041f141d1b322c94
Zhanett Serikbayeva <109654778+zhanettprog@users.noreply.github.com>
2022-09-05 22:35:26 +0600

    Add files via upload

---
+++ 90.js
@@ -0,0 +1,11 @@
+const userProfile = {
+  name: "Zhanett",
+  commentsQty: 31,
+  hasSignedAgreement: false,
+};
+
+const { name, commentsQty } = userProfile;
+const { hasSignedAgreement } = userProfile;
+
+console.log(name);
+console.log(commentsQty);
