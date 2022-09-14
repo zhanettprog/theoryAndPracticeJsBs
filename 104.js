1e986ee1701073edd0d3d4bd179210ca3963606f
Zhanett Serikbayeva <109654778+zhanettprog@users.noreply.github.com>
2022-09-14 20:51:06 +0600

    Add files via upload

---
+++ 104.js
@@ -0,0 +1,13 @@
+const age = 5;
+
+if (age >= 18) {
+  console.log("Is adult");
+}
+
+if (age >= 12 && age < 18) {
+  console.log("Is teenager");
+}
+
+if (age < 12) {
+  console.log("Is child");
+}
