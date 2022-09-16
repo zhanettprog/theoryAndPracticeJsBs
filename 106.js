061ea9f34cb67032b4665e7dc0cc3db713ecd50d
Zhanett Serikbayeva <109654778+zhanettprog@users.noreply.github.com>
2022-09-16 22:00:16 +0600

    Add files via upload

---
+++ 106.js
@@ -0,0 +1,15 @@
+const month = 5;
+
+switch (month) {
+  case 12:
+    console.log("Декабрь");
+    break;
+  case 1:
+    console.log("Январь");
+    break;
+  case 2:
+    console.log("Февраль");
+    break;
+  default:
+    console.log("Это не зимний месяц");
+}
