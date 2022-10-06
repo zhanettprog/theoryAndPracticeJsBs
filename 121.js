e725e6b9e31711e25010375e9547b120df32fcec
Zhanett Serikbayeva <109654778+zhanettprog@users.noreply.github.com>
2022-10-06 22:03:15 +0600

    Update 121.js

--- 121.js
+++ 121.js
@@ -1,15 +1,6 @@
-const month = 5;
+let i = 10;
 
-switch (month) {
-  case 12:
-    console.log("Декабрь");
-    break;
-  case 1:
-    console.log("Январь");
-    break;
-  case 2:
-    console.log("Февраль");
-    break;
-  default:
-    console.log("Это не зимний месяц");
-}
+do {
+  console.log(i);
+  i++;
+} while (i < 5);
